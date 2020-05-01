150497464c0b68785e6d6f9bf8b9eed3d3a37d42
Calbar01 <56161940+Calbar01@users.noreply.github.com>
2020-05-01 13:14:35 +0100

    commenting

--- scripts.js
+++ scripts.js
@@ -16,10 +16,10 @@
 
 // /* SCRIPTS.JS */
 
-// function changePic() {
-//     document.getElementById("profilePic").src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian-prp.jpg";
-//   }
+ function changePic() {
+     document.getElementById("profilePic").src = "https://source.unsplash.com/random";
+   }
   
-//   function normalPic() {
-//     document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
-//   }
+   function normalPic() {
+     document.getElementById("profilePic") .src = "/images/Me.jpg";
+   }
